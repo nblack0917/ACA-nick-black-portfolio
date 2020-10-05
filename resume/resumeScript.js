5c3432db01dbf7b5734e44a51545cb4dcc6a3236
nblack0917 <nickablack@gmail.com>
2020-10-05 14:32:31 -0500

    added fadeOut to phone number

--- resume/resumeScript.js
+++ resume/resumeScript.js
@@ -1,14 +1,16 @@
 let click = false;
+let photoClick = false;
 
 const fadeInName = () => {
     const name = document.getElementById("main-name");
-    if (!click){ console.log("click")
-    name.classList.add("animate__animated", "animate__fadeIn", "animate__slower");
-    click = true;
-    } else {
-        name.classList.remove("animate__fadeIn", "animate__slower");
-        name.classList.add("animate__backInUp");
-    click = false;
+    if (!click){
+        console.log("click")
+        name.classList.add("animate__animated", "animate__fadeIn", "animate__slower");
+        click = true;
+        } else {
+            name.classList.remove("animate__fadeIn", "animate__slower");
+            name.classList.add("animate__backInUp");
+        click = false;
     }
 }
 
@@ -25,6 +27,15 @@ function fadeInPic() {
 
 function showPhone() {
     const phone = document.getElementById("phoneNum");
-    phone.style.visibility = "visible";
-    phone.classList.add("animate__animated", "animate__fadeIn")
-}
+    if (!photoClick) {
+        phone.style.visibility = "visible";
+        phone.classList.remove("animate__fadeOut")
+        phone.classList.add("animate__animated", "animate__fadeIn")
+        photoClick = true
+        } else {
+            phone.classList.remove("animate__fadeIn")
+            phone.classList.add("animate__fadeOut", "animate__slow")
+            photoClick = false;
+            
+        } 
+    }
